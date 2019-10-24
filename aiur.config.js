a2061543251605b17f1f6c002eda4bd5a3ec09a8
Joy Clark <joy.clark@innoq.com>
2019-10-24 15:07:47 +0200

    Fixing the SCSS (import bootstrap from source)

--- aiur.config.js
+++ aiur.config.js
@@ -11,15 +11,6 @@ exports.js = [{
   source: "./index.js"
 }];
 
-exports.vendor = {
-	styles: [{
-		crossorigin: "anonymous",
-		integrity: "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T",
-		href: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-	}],
-	scripts: []
-};
-
 exports.pages = {
 	"": "./components/welcome.md",
 	components: {
