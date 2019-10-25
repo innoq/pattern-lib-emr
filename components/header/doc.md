c94ece8d3478b4c87f784de2902f37b53fe366a6
Franziska Dessart <franziska.dessart@innoq.com>
2019-10-25 15:17:25 +0200

    fix header: when not all values exist

--- components/header/doc.md
+++ components/header/doc.md
@@ -6,11 +6,21 @@ The Header.
 import { Header } from "./components/header";
 import { createElement } from "complate-stream";
 
-<Header name="Amlesu Ncube"
+<Header firstname="Amlesu"
+        lastname="Ncube"
         sex="female"
         birthday="1970-01-01"
         age="85"
         birthplace="Bavaria/Nuremberg"
         address="Bavaria/Munich" 
 />
+```
+
+The Header, when only name is known.
+
+```jsx
+import { Header } from "./components/header";
+import { createElement } from "complate-stream";
+
+<Header firstname="Amlesu" lastname="Ncube" />
 ```
--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,13 +1,17 @@
 import { createElement } from "complate-stream";
 
-export function Header({ name, sex, birthday, age, birthplace, address}) {
+export function Header({ firstname, lastname, sex, birthday, age, birthplace, address}) {
 	return <header is="emr-header" class="emr-header">
-		<div class="emr-header-element emr-header-element__name">{name}</div>
+		<div class="emr-header-element emr-header-element__name">{firstname} {lastname}</div>
 		<div class="emr-header-element">{sex}</div>
 		<div class="emr-header-element">
-			<div class="">{birthday}</div><div>{age}&nbsp;yrs</div>
+			<div>{birthday}</div><div>{age &&  `${age} yrs`}</div>
 		</div>
-		<div class="emr-header-element emr-header-element__address"><span class="fas fa-star-of-life emr-header-element__address__icon"></span>{birthplace}</div>
-		<div class="emr-header-element emr-header-element__address"><span class="fas fa-home emr-header-element__address__icon"></span>{address}</div>
+		<div class="emr-header-element emr-header-element__address">
+			{birthplace && [<span class="fas fa-star-of-life emr-header-element__address__icon"></span>, birthplace]}
+		</div>
+		<div class="emr-header-element emr-header-element__address">
+			{address && [<span class="fas fa-home emr-header-element__address__icon"></span>, address]}
+		</div>
 	</header>;
 }
