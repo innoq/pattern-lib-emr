5e2cff35272da745f0014b71c37ef4f80badaa2c
Joy Clark <joy.clark@innoq.com>
2019-10-25 11:23:13 +0200

    Changes to icon (add IconAfter template and don't add label within the emr-icon class (otherwise the font-style is off))

--- components/icon-element/index.jsx
+++ components/icon-element/index.jsx
@@ -1,5 +1,15 @@
 import { createElement } from "complate-stream";
 
 export function IconElement({ icon, label }) {
-	return <span class={`emr-icon fa fa-${icon}`}>{label}</span>;
+	return <span class="emr-icon">
+			<i class={`fa fa-${icon}`}></i>
+			{label}
+		</span>;
+}
+
+export function IconAfter({ icon, label }) {
+	return <span class="emr-icon-after">
+		{label}
+		<i class={`fa fa-${icon}`}></i>
+	</span>;
 }
